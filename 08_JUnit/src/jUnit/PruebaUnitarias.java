7b7a77aa05ea921b6e86f2d6fc5ad3ebc739e651
rbaluta17dw <ik012982i9@206-10-HZ336130.H012982.NET>
2018-02-09 09:46:39 +0100

    cambio pequeño

--- 08_JUnit/src/jUnit/PruebaUnitarias.java
+++ 08_JUnit/src/jUnit/PruebaUnitarias.java
@@ -3,18 +3,28 @@ package jUnit;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import batalla.Luchador;
 import vista.LuchaMenu;
 
 public class PruebaUnitarias {
 
 	@Test(expected = NumberFormatException.class)
-	public void test() {
+	public void test() throws Exception {
 		LuchaMenu test = new LuchaMenu();
 		test.lanzarMenu();
 	}
+
 	@Test
 	public void test2() {
-		LuchaMenu test = new LuchaMenu();
-		test.lanzarMenu();
+		try {
+			LuchaMenu test = new LuchaMenu();
+			test.lanzarMenu();
+			fail("se esperaba un error");
+		} catch (NumberFormatException e) {
+			
+		}
 	}
+	
+	
 }
